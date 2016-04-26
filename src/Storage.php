f65846ad9bbf95052fa4cc3f29f30a83adb3e3a6
ErickSkrauch <erickskrauch@yandex.ru>
2016-04-26 23:29:38 +0300

    Initial commit: base git repo structure, base Storage implementation and tests

---
+++ src/Storage.php
@@ -0,0 +1,49 @@
+<?php
+namespace Ely\TempMailBuster;
+
+class Storage
+{
+    /**
+     * @var array of string, which contains masks for temp mail services
+     */
+    private $blacklist;
+
+    /**
+     * @return array with current blacklist
+     */
+    public function getBlacklist()
+    {
+        return $this->blacklist;
+    }
+
+    /**
+     * @param string|array $items item or items, that will be merged to blacklist
+     * @return static
+     */
+    public function appendToBlacklist($items)
+    {
+        $items = (array)$items;
+        $this->blacklist = array_merge($this->blacklist, $items);
+
+        return $this;
+    }
+
+    /**
+     * @param array $items override current blacklist with passed values
+     * @return static
+     */
+    public function setBlacklist(array $items)
+    {
+        $this->blacklist = $items;
+
+        return $this;
+    }
+
+    /**
+     * @param array $blacklist
+     */
+    public function __construct(array $blacklist = [])
+    {
+        $this->blacklist = $blacklist;
+    }
+}
---
+++ tests/StorageTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ely\TempMailBuster;
+
+class StorageTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetBlackList()
+    {
+        $storage = new Storage(['item']);
+        $this->assertEquals(['item'], $storage->getBlacklist());
+    }
+
+    public function testAppendToBlacklist()
+    {
+        $storage = new Storage(['item1']);
+        $storage->appendToBlacklist(['item2']);
+        $this->assertEquals(['item1', 'item2'], $storage->getBlacklist());
+
+        $storage = new Storage(['item1']);
+        $storage->appendToBlacklist('item2');
+        $this->assertEquals(['item1', 'item2'], $storage->getBlacklist());
+    }
+
+    public function testSetBlacklist()
+    {
+        $storage = new Storage(['item1']);
+        $storage->setBlacklist(['item2']);
+        $this->assertEquals(['item2'], $storage->getBlacklist());
+    }
+}
